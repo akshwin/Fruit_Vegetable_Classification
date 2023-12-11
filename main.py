14c11e79e00d86dd73e79611f2430cce43ed88cb
akshwin tamilselvan <akshwint.2003@gmail.com>
2023-12-11 20:41:35 +0530

    added streamlit app

--- main.py
+++ main.py
@@ -7,20 +7,9 @@ import requests
 from bs4 import BeautifulSoup
 
 model = load_model("FruitModel.h5")
-labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot', 7: 'cauliflower', 8: 'chilli pepper', 9: 'corn', 10: 'cucumber', 11: 'eggplant', 12: 'garlic', 13: 'ginger', 14: 'grapes', 15: 'jalepeno', 16: 'kiwi', 17: 'lemon', 18: 'lettuce', 19: 'mango', 20: 'onion', 21: 'orange',22: 'paprika', 23: 'pear', 24: 'peas', 25: 'pineapple',26: 'pomegranate', 27: 'potato', 28: 'raddish', 29: 'soy beans', 30: 'spinach', 31: 'sweetcorn', 32: 'sweetpotato', 33: 'tomato', 34: 'turnip', 35: 'watermelon'}
-fruits = {'banana', 'apple', 'pear', 'grapes', 'orange', 'kiwi', 'watermelon', 'pomegranate', 'pineapple', 'mango'}
-vegetables={'cucumber', 'carrot', 'capsicum', 'onion', 'Potato', 'lemon', 'tomato', 'raddish', 'beetroot', 'cabbage', 'lettuce', 'spinach', 'soy bean', 'cauliflower', 'bell pepper', 'chilli pepper', 'turnip', 'corn', 'sweetcorn', 'sweet potato', 'paprika', 'jalepeño', 'ginger', 'garlic', 'peas','eggplant'}
-
-def fetch_calorie(prediction):
-    try:
-        url ='https://www.google.com/search?&q=calories in ' + prediction
-        req = requests.get(url).text
-        scrap = BeautifulSoup(req,'html.parser')
-        calories = scrap.find("div", class_="BNeawe iBp4i AP7Wnd").text
-        return calories
-    except Exception as  e:
-        st.error("Sorry !! Calorie not found")
-        print (e)
+labels = {0: 'Apple', 1: 'Banana', 2: 'Beetroot', 3: 'Bell pepper', 4: 'Cabbage', 5: 'Capsicum', 6: 'Carrot', 7: 'Cauliflower', 8: 'Chilli pepper', 9: 'Corn', 10: 'Cucumber', 11: 'Eggplant', 12: 'Garlic', 13: 'Ginger', 14: 'Grapes', 15: 'Jalepeno', 16: 'Kiwi', 17: 'Lemon', 18: 'Lettuce', 19: 'Mango', 20: 'Onion', 21: 'Orange',22: 'Paprika', 23: 'Pear', 24: 'Peas', 25: 'Pineapple',26: 'Pomegranate', 27: 'Potato', 28: 'Raddish', 29: 'Soy beans', 30: 'Spinach', 31: 'Sweetcorn', 32: 'Sweetpotato', 33: 'Tomato', 34: 'Turnip', 35: 'Watermelon'}
+fruits = {'Banana', 'Apple', 'Pear', 'Grapes', 'Orange', 'Kiwi', 'Watermelon', 'Pomegranate', 'Pineapple', 'Mango'}
+vegetables={'Cucumber', 'Carrot', 'Capsicum', 'Onion', 'Potato', 'Lemon', 'Tomato', 'Raddish', 'Beetroot', 'Cabbage', 'Lettuce', 'Spinach', 'Soy bean', 'Cauliflower', 'Bell pepper', 'Chilli pepper', 'Turnip', 'Corn', 'Sweetcorn', 'Sweet potato', 'Paprika', 'Jalepeño', 'Ginger', 'Garlic', 'Peas','Eggplant'}
 
 def processed_img(img_path):
     img=load_img(img_path,target_size=(224,224,3))
@@ -48,12 +37,9 @@ def run():
         if img_file is not None :
             result = processed_img(save_image_path)
             if result in vegetables:
-                st.info('**Category : Vegetables**')
+                st.info('**Category : Vegetable**')
             else :
-                st.info('**Category : Fruits**')
+                st.info('**Category : Fruit**')
             
             st.success("**Predicted : "+  result+"**")
-            cal = fetch_calorie(result)
-            if cal :
-                st.warning('**'+cal+'**')
 run()
