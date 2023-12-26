69e188050ccc101ae5e0693b14c262cafe2539dc
akshwin tamilselvan <akshwint.2003@gmail.com>
2023-12-26 15:21:20 +0530

    added main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from PIL import Image
 from tensorflow.keras.utils import load_img,img_to_array
 import numpy as np
 from keras.models import load_model
-import requests
-from bs4 import BeautifulSoup
 
 model = load_model("FruitModel.h5")
 labels = {0: 'Apple', 1: 'Banana', 2: 'Beetroot', 3: 'Bell pepper', 4: 'Cabbage', 5: 'Capsicum', 6: 'Carrot', 7: 'Cauliflower', 8: 'Chilli pepper', 9: 'Corn', 10: 'Cucumber', 11: 'Eggplant', 12: 'Garlic', 13: 'Ginger', 14: 'Grapes', 15: 'Jalepeno', 16: 'Kiwi', 17: 'Lemon', 18: 'Lettuce', 19: 'Mango', 20: 'Onion', 21: 'Orange',22: 'Paprika', 23: 'Pear', 24: 'Peas', 25: 'Pineapple',26: 'Pomegranate', 27: 'Potato', 28: 'Raddish', 29: 'Soy beans', 30: 'Spinach', 31: 'Sweetcorn', 32: 'Sweetpotato', 33: 'Tomato', 34: 'Turnip', 35: 'Watermelon'}
