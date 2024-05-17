de37ef8a26d328dc929997cca9cbb9419a317350
akshwin tamilselvan <akshwint.2003@gmail.com>
2024-05-17 21:41:35 +0530

    added model

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tensorflow.keras.utils import load_img,img_to_array
 import numpy as np
 from keras.models import load_model
 
-model = load_model("FruitModel.h5")
+model = load_model("FruitModel.keras")
 labels = {0: 'Apple', 1: 'Banana', 2: 'Beetroot', 3: 'Bell pepper', 4: 'Cabbage', 5: 'Capsicum', 6: 'Carrot', 7: 'Cauliflower', 8: 'Chilli pepper', 9: 'Corn', 10: 'Cucumber', 11: 'Eggplant', 12: 'Garlic', 13: 'Ginger', 14: 'Grapes', 15: 'Jalepeno', 16: 'Kiwi', 17: 'Lemon', 18: 'Lettuce', 19: 'Mango', 20: 'Onion', 21: 'Orange',22: 'Paprika', 23: 'Pear', 24: 'Peas', 25: 'Pineapple',26: 'Pomegranate', 27: 'Potato', 28: 'Raddish', 29: 'Soy beans', 30: 'Spinach', 31: 'Sweetcorn', 32: 'Sweetpotato', 33: 'Tomato', 34: 'Turnip', 35: 'Watermelon'}
 fruits = {'Banana', 'Apple', 'Pear', 'Grapes', 'Orange', 'Kiwi', 'Watermelon', 'Pomegranate', 'Pineapple', 'Mango'}
 vegetables={'Cucumber', 'Carrot', 'Capsicum', 'Onion', 'Potato', 'Lemon', 'Tomato', 'Raddish', 'Beetroot', 'Cabbage', 'Lettuce', 'Spinach', 'Soy bean', 'Cauliflower', 'Bell pepper', 'Chilli pepper', 'Turnip', 'Corn', 'Sweetcorn', 'Sweet potato', 'Paprika', 'Jalepeño', 'Ginger', 'Garlic', 'Peas','Eggplant'}
