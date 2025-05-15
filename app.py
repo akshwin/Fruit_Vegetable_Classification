3775418b406f51f7ffb6d70a9d52620c251421f3
akshwin tamilselvan <akshwint.2003@gmail.com>
2025-05-15 22:55:21 +0530

    added model

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ vegetables = {
 
 # Classify uploaded image
 def classify_image(img_path):
-    img = load_img(img_path, target_size=(224, 224))
+    img = load_img(img_path, target_size=(224, 224, 3))
     img_array = img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
     prediction = model.predict(img_array)
